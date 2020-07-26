e04a8a91964a53e1d2bf631f9f37f1435b68904c
riadkanj <riadkanj@gmail.com>
2020-07-26 23:28:14 +0300

    changed_section_about

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <Fragment>
       <h1>About this App</h1>
-      <p>A website to find top educational content for your topic of interest</p>
+      <p>An app to find top educational content for your topic of interest</p>
       <br />
       <p>Version: 1.0.0</p>
     </Fragment>
